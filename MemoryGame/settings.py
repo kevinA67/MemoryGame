c3b1158fbe347731346b80523e5a93dc126ca6df
kevinA67 <kfunes599@gmail.com>
2025-07-20 09:20:31 -0600

    Conexión a la base de datos MYSQL

--- MemoryGame/settings.py
+++ MemoryGame/settings.py
@@ -73,12 +73,24 @@ WSGI_APPLICATION = 'MemoryGame.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mydb',
+        'USER': 'myuser',
+        'PASSWORD': 'mypassword',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
+
 
 
 # Password validation
